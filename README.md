734be56576b51030a4100295d10d36beecee0e70
Hamin Park <haeeema@naver.com>
2023-08-16 23:20:17 +0900

    indeed job scrapper part

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # A Bypass using Selenium
 
-The Bypass Bot is a Python script designed to automate browser interactions using the `selenium-stealth` and `selenium-wire` libraries, utilizing a detected Chrome browser instance. This bot is created to navigate websites while evading detection mechanisms, providing a versatile tool for various web scraping, automation, or testing tasks.
+The Bypass Bot is a Python script designed to automate browser interactions using the `selenium-wire` libraries, utilizing a detected Chrome browser instance. This bot is created to navigate websites while evading detection mechanisms, providing a versatile tool for various web scraping, automation, or testing tasks.
 
 ## Features
 
-- **Stealthy Browsing**: The bot employs `selenium-stealth` to mimic human-like browsing behavior, including user agents, languages, and other attributes, reducing the chances of being detected as a bot.
-
 - **Network Manipulation**: Utilizing `selenium-wire`, the bot has the ability to intercept and modify network requests, which can be useful for debugging, data manipulation, or other advanced use cases.
+
+- **Stealthy Browsing**: The bot employs `selenium-stealth` to mimic human-like browsing behavior, including user agents, languages, and other attributes, reducing the chances of being detected as a bot.
 
 - **Detected Chrome Browser**: The bot leverages a detected Chrome browser instance, enabling compatibility with websites that use anti-bot mechanisms to detect automated browser sessions.
 
@@ -24,6 +24,7 @@ The Bypass Bot is a Python script designed to automate browser interactions usin
 ```bash
 pip3 install selenium selenium-stealth selenium-wire undetected-chromedriver
 ```
+
 ## Notion
 
 [✅ Selenium_Bypass](https://www.notion.so/haminpark/Selenium_Bypass-Cloudflare-f4fb5934e63147c29d1623e6de556e0f)
--- main.py
+++ main.py
@@ -1,55 +1,63 @@
 from bs4 import BeautifulSoup
 
-from seleniumwire import webdriver
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-base_url = "https://kr.indeed.com/jobs?q="
-search_term = "python"
 
-options = Options()
-'''
-options.add_argument('window-size=1920x1080')
-options.add_argument("disable-gpu")
-options.add_argument("lang=ko_KR")
-options.add_argument(
-    'user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
-options.add_argument("start-maximized")
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_experimental_option('useAutomationExtension', False)
-'''
-# options.add_argument('--headless')
+def get_page_count(keyword):
+    base_url = "https://kr.indeed.com/jobs?q="
+    options = Options()
+    browser = webdriver.Chrome(options=options)
+    browser.get(f"{base_url}{keyword}")
 
-browser = webdriver.Chrome(options=options)
-
-browser.get(f"{base_url}{search_term}")
-
-response = browser.last_request.response
-
-if response.status_code != 200:
-    print("Can't request page")
-else:
     soup = BeautifulSoup(browser.page_source, "html.parser")
-    jobs_list = soup.find("ul", class_="jobsearch-ResultsList")
-    jobs = jobs_list.find_all("li", recursive=False)
-    # Beautifulsoup find all of "li"
-    # We only want to search for "li"s that are directly under ul tag.
+    pagination = soup.find("nav", class_="ecydgvn0")
+    if pagination == None:
+        return 1
+    pages = pagination.find_all("div", recursive=False)
+    count = len(pages)
+    if count >= 5:
+        return 5
+    else:
+        return count
+
+
+def extract_indeed_jobs(keyword):
+    pages = get_page_count(keyword)
     results = []
-    for job in jobs:
-        zone = job.find("div", class_="mosaic-zone")
-        if zone == None:
-            # h2 = job.find("h2", class_="jobTitle")
-            # a = h2.find("a")
-            anchor = job.select_one("h2 a")
-            title = anchor["aria-label"]
-            link = anchor["href"]
-            company = job.find("span", class_="companyName")
-            location = job.find("div", class_="companyLocation")
-            job_data = {
-                "link": f"https://kr.indeed.com{link}",
-                "company": company.string,
-                "location": location.string,
-                "position": title,
-            }
-            results.append(job_data)
-    for result in results:
-        print(result, "\n/////////////\n")
+    for page in range(pages):
+        base_url = "https://kr.indeed.com/jobs"
+        options = Options()
+        browser = webdriver.Chrome(options=options)
+        final_url = f"{base_url}?q={keyword}&start={page*10}"
+        browser.get(final_url)
+
+        soup = BeautifulSoup(browser.page_source, "html.parser")
+        jobs_list = soup.find("ul", class_="jobsearch-ResultsList")
+        jobs = jobs_list.find_all("li", recursive=False)
+        # Beautifulsoup find all of "li"
+        # We only want to search for "li"s that are directly under ul tag.
+        for job in jobs:
+            zone = job.find("div", class_="mosaic-zone")
+            if zone == None:
+                # h2 = job.find("h2", class_="jobTitle")
+                # a = h2.find("a")
+                anchor = job.select_one("h2 a")
+                title = anchor["aria-label"]
+                link = anchor["href"]
+                company = job.find("span", class_="companyName")
+                location = job.find("div", class_="companyLocation")
+                job_data = {
+                    "link": f"https://kr.indeed.com{link}",
+                    "company": company.string,
+                    "location": location.string,
+                    "position": title,
+                }
+                results.append(job_data)
+    return results
+
+
+jobs = extract_indeed_jobs("python")
+
+
+print(jobs)
