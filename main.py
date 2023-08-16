1cfc1a0815a7f7da1fdd284c3aa7e34f0a182f35
Hamin Park <haeeema@naver.com>
2023-08-16 23:47:11 +0900

    Extractor

--- main.py
+++ main.py
@@ -1,63 +1,8 @@
-from bs4 import BeautifulSoup
+from indeed import extract_indeed_jobs
+from wwr import extract_wwr_jobs
 
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+keyword = input("What do you want to search for?")
 
-
-def get_page_count(keyword):
-    base_url = "https://kr.indeed.com/jobs?q="
-    options = Options()
-    browser = webdriver.Chrome(options=options)
-    browser.get(f"{base_url}{keyword}")
-
-    soup = BeautifulSoup(browser.page_source, "html.parser")
-    pagination = soup.find("nav", class_="ecydgvn0")
-    if pagination == None:
-        return 1
-    pages = pagination.find_all("div", recursive=False)
-    count = len(pages)
-    if count >= 5:
-        return 5
-    else:
-        return count
-
-
-def extract_indeed_jobs(keyword):
-    pages = get_page_count(keyword)
-    results = []
-    for page in range(pages):
-        base_url = "https://kr.indeed.com/jobs"
-        options = Options()
-        browser = webdriver.Chrome(options=options)
-        final_url = f"{base_url}?q={keyword}&start={page*10}"
-        browser.get(final_url)
-
-        soup = BeautifulSoup(browser.page_source, "html.parser")
-        jobs_list = soup.find("ul", class_="jobsearch-ResultsList")
-        jobs = jobs_list.find_all("li", recursive=False)
-        # Beautifulsoup find all of "li"
-        # We only want to search for "li"s that are directly under ul tag.
-        for job in jobs:
-            zone = job.find("div", class_="mosaic-zone")
-            if zone == None:
-                # h2 = job.find("h2", class_="jobTitle")
-                # a = h2.find("a")
-                anchor = job.select_one("h2 a")
-                title = anchor["aria-label"]
-                link = anchor["href"]
-                company = job.find("span", class_="companyName")
-                location = job.find("div", class_="companyLocation")
-                job_data = {
-                    "link": f"https://kr.indeed.com{link}",
-                    "company": company.string,
-                    "location": location.string,
-                    "position": title,
-                }
-                results.append(job_data)
-    return results
-
-
-jobs = extract_indeed_jobs("python")
-
-
-print(jobs)
+indeed = extract_indeed_jobs(keyword)
+wwr = extract_wwr_jobs(keyword)
+jobs = indeed + wwr
---
+++ wwr.py
@@ -0,0 +1,38 @@
+from requests import get
+from bs4 import BeautifulSoup
+
+
+def extract_wwr_jobs(keyword):
+    base_url = "https://weworkremotely.com/remote-jobs/search?term="
+
+    response = get(f"{base_url}{keyword}")
+
+    if response.status_code != 200:
+        print("Can't request website")
+    else:
+        results = []
+        soup = BeautifulSoup(response.text, "html.parser")
+        # "html.parser" tells Beautifulsoup to send HTML.
+        jobs = soup.find_all("section", class_="jobs")
+        # Find all the section that have the class of jobs.
+        # class_="jobs" is keyword argument.
+        for job_section in jobs:
+            job_posts = job_section.find_all("li")
+            job_posts.pop(-1)
+            # pop method is for removing view-all list, it is located on the last of the list.
+            for post in job_posts:
+                anchors = post.find_all("a")
+                anchor = anchors[1]
+                link = anchor["href"]
+                company, kind, region = anchor.find_all(
+                    "span", class_="company")
+                # Shortcut
+                title = anchor.find("span", class_="title")
+                job_data = {
+                    "link": f"https://weworkremotely.com{link}",
+                    "company": company.string,
+                    "location": region.string,
+                    "position": title.string,
+                }
+                results.append(job_data)
+        return results
